4b4b1dea2a61b90f36e5d5a60ab6e44d8f3b7921
Minh Quang Pham <minhquang@chmtlp31.limsi.fr>
2020-05-08 09:55:56 +0200

    debug

--- translate.py
+++ translate.py
@@ -2,44 +2,53 @@ import numpy as np
 import os
 import argparse
 import ctranslate2
+import yaml
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('-s', '--src', required=True, help="Source file path")
-    argparser.add_argument('-t', '--tgt', required=True, help="Target file path")
+    argparser.add_argument('--config', required=False, help="config")
+    argparser.add_argument('-s', '--src', required=False, help="Source file path")
+    argparser.add_argument('-t', '--tgt', required=False, help="Target file path")
     argparser.add_argument('-m', '--model', required=True, help="Model path")
     argparser.add_argument('-j', '--joiner', default='‖', help="Sentence joiner")
     argparser.add_argument('-d', '--dev', default='cpu', choices=["cpu", "cuda", "auto"], help="Device to translate")
     argparser.add_argument('--hyp', required=True, help="Output translation file name")
     args = argparser.parse_args()
-    src = [l.split() for l in open(args.src)]
-    tgt_all = [l for l in open(args.tgt)]
-    prefix = []
-    for i in range(len(tgt_all)):
-        if args.joiner in tgt_all[i]:
-            pre = " ".join([tgt_all[i], args.joiner])
-            prefix.append(pre.split())
-        else:
-            prefix.append(None)
+    with open(args.config,"r") as stream:
+        config = yaml.load(stream)
+
+    for src, tgt in zip(config["src"], config["tgt"]):
+        print("translate %s with prefix %s"%(src,tgt))
+        with open(src,"r") as f:
+            src = [l.strip().split() for l in f.readlines()]
+        with open(tgt,"r") as f:
+            tgt_all = [l.strip() for l in f.readlines()]
+        prefix = []
+        for i in range(len(tgt_all)):
+            if args.joiner in tgt_all[i]:
+                pre = " ".join([tgt_all[i], args.joiner])
+                prefix.append(pre.split())
+            else:
+                prefix.append(None)
 
 
-    translator = ctranslate2.Translator(
-            model_path=args.model,
-            device=args.dev,
-            device_index=0)
+        translator = ctranslate2.Translator(
+                model_path=args.model,
+                device=args.dev,
+                device_index=0)
 
-    output = translator.translate_batch(
-            source=src,
-            target_prefix=prefix,
-            max_batch_size=60,
-            beam_size=5,
-            length_penalty=0.2,
-            min_decoding_length=0,
-            return_scores=False)
+        output = translator.translate_batch(
+                source=src,
+                target_prefix=prefix,
+                max_batch_size=60,
+                beam_size=5,
+                length_penalty=0.2,
+                min_decoding_length=0,
+                return_scores=False)
 
-    with open(args.hyp, "w") as f:
-        for batch in output:
-            for line in batch:
-                hyp = " ".join(line["tokens"])
-                # f.write(" ".join(line["tokens"]) + "\n")
-                f.write(hyp.split(" %s " % args.joiner)[-1] + "\n")
+        with open(args.hyp, "w") as f:
+            for batch in output:
+                for line in batch:
+                    hyp = " ".join(line["tokens"])
+                    # f.write(" ".join(line["tokens"]) + "\n")
+                    f.write(hyp.split(" %s " % args.joiner)[-1] + "\n")
